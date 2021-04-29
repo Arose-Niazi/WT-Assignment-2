083278a86f601cd3f3c0b597c6742347bfb950c0
Arose Niazi <arose.niazi@yahoo.com>
2021-04-29 18:04:07 +0500

    Indent Fixed

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 const NAME = 0;
 const GENDER = 1;
 const AGE = 2;
-const CITY = 3; 
-
+const CITY = 3;
 
 var CanDelete = true;
 var CurrentEditing;
-var NamePattern = new RegExp("(^[A-Za-z]{1,10})+$");//Only allow Alphabets with size between 1 to 10. 
+var NamePattern = new RegExp("(^[A-Za-z]{1,10})+$"); //Only allow Alphabets with size between 1 to 10.
 
 // Buttons
 var updateButton;
@@ -21,24 +20,20 @@ var maleInput;
 var femaleInput;
 var cityInput;
 
-
 // On Load
-window.onload = function()
-{
+window.onload = function () {
     AssignVariables();
 
     Reset();
-    
+
     //On Click Binding
     resetButton.onclick = Reset;
     updateButton.onclick = UpdateValues;
     addButton.onclick = Add;
-}
-
+};
 
 // Functions
-function AssignVariables()
-{
+function AssignVariables() {
     // Buttons
     updateButton = document.getElementById("Update");
     resetButton = document.getElementById("Reset");
@@ -51,15 +46,12 @@ function AssignVariables()
     femaleInput = document.getElementById("Female");
     cityInput = document.getElementById("City");
 
-    //Table Body where we will insert data. 
+    //Table Body where we will insert data.
     tableBody = document.getElementById("Persons");
-
 }
 
-function Reset()
-{
-
-    updateButton.disabled = true //This will diable the update button
+function Reset() {
+    updateButton.disabled = true; //This will diable the update button
     addButton.disabled = false; //This will enable the add button
 
     //Resetting the values in form to a default value.
@@ -71,87 +63,89 @@ function Reset()
     CanDelete = true;
 }
 
-function Update(e)
-{
+function Update(e) {
     var currentRow = e.target.parentNode.parentNode; //Getting parent of 'a' -> 'td' -> 'tr' So we got the row
-    CurrentEditing = []; //Stores the childs 'td' we need 
+    CurrentEditing = []; //Stores the childs 'td' we need
 
-    for (let index = 0; index < currentRow.childNodes.length; index++) { //Loop through all childs of currentRow
+    for (let index = 0; index < currentRow.childNodes.length; index++) {
+        //Loop through all childs of currentRow
         const element = currentRow.childNodes[index]; //Getting the child.
-        if(element.tagName == undefined) continue; //If that element is undefined that means, it's extra we don't need it.
-        CurrentEditing.push(element) //Add the childs in currrent Editng list. 
+        if (element.tagName == undefined) continue; //If that element is undefined that means, it's extra we don't need it.
+        CurrentEditing.push(element); //Add the childs in currrent Editng list.
     }
 
     //Updating the form with the values
-    nameInput.value = CurrentEditing[NAME].innerText; 
-    ageInput.value = CurrentEditing[AGE].innerText; 
-    cityInput.value = CurrentEditing[CITY].innerText; 
+    nameInput.value = CurrentEditing[NAME].innerText;
+    ageInput.value = CurrentEditing[AGE].innerText;
+    cityInput.value = CurrentEditing[CITY].innerText;
 
     //Gender checking
-    if(CurrentEditing[GENDER].innerText === "Male")
-        maleInput.checked = true;  //Check the male radio button
-    else
-        femaleInput.checked = true; //Check the female radio button
+    if (CurrentEditing[GENDER].innerText === "Male") maleInput.checked = true;
+    //Check the male radio button
+    else femaleInput.checked = true; //Check the female radio button
 
-    addButton.disabled = true; //Now adding new value is disabled. 
-    updateButton.disabled = false; //Enable the update button. 
+    addButton.disabled = true; //Now adding new value is disabled.
+    updateButton.disabled = false; //Enable the update button.
 
-    CanDelete = false; //As we are editing a value, can not delete anymore. 
+    CanDelete = false; //As we are editing a value, can not delete anymore.
 }
 
-function Delete(e)
-{
-    if(!CanDelete) return; 
-    e.target.parentNode.parentNode.remove(); //Getting parent of 'a' -> 'td' -> 'tr' So we got the row and remove() will delete it. 
+function Delete(e) {
+    if (!CanDelete) return;
+    e.target.parentNode.parentNode.remove(); //Getting parent of 'a' -> 'td' -> 'tr' So we got the row and remove() will delete it.
 }
 
-function UpdateValues()
-{
-    //Input validation 
-    if(!IsValidInput()) return; 
+function UpdateValues() {
+    //Input validation
+    if (!IsValidInput()) return;
 
-    //Updating the values in table. 
+    //Updating the values in table.
     CurrentEditing[NAME].innerText = nameInput.value;
     CurrentEditing[AGE].innerText = ageInput.value;
     CurrentEditing[CITY].innerText = cityInput.value;
-    if(maleInput.checked)
-        CurrentEditing[GENDER].innerText = "Male";
-    else
-        CurrentEditing[GENDER].innerText = "Female";
+    if (maleInput.checked) CurrentEditing[GENDER].innerText = "Male";
+    else CurrentEditing[GENDER].innerText = "Female";
 }
 
-function Add()
-{
-    //Input validation 
-    if(!IsValidInput()) return; 
+function Add() {
+    //Input validation
+    if (!IsValidInput()) return;
 
     //If male radio button is checked then male else female.
-    var gender = (maleInput.checked)?"Male":"Female";
+    var gender = maleInput.checked ? "Male" : "Female";
 
-    //Adding new row to table body 
-    tableBody.innerHTML += 
-    '<tr>'+
-        '<th scope="row">'+nameInput.value+'</th>'+
-        '<td>'+gender+'</td>'+
-        '<td>'+ageInput.value+'</td>'+
-        '<td>'+cityInput.value+'</td>'+
-        '<td><a href="#" onclick="Update(event)">Update</a> / <a href="#" onclick="Delete(event)">Delete</a></td>'+
-    '</tr>';
+    //Adding new row to table body
+    tableBody.innerHTML +=
+        "<tr>" +
+        '<th scope="row">' +
+        nameInput.value +
+        "</th>" +
+        "<td>" +
+        gender +
+        "</td>" +
+        "<td>" +
+        ageInput.value +
+        "</td>" +
+        "<td>" +
+        cityInput.value +
+        "</td>" +
+        '<td><a href="#" onclick="Update(event)">Update</a> / <a href="#" onclick="Delete(event)">Delete</a></td>' +
+        "</tr>";
 
-    //Reseting the form. 
+    //Reseting the form.
     Reset();
 }
 
 function IsValidInput() {
     //Matches name with regex pattern
-    if(!NamePattern.test(nameInput.value))
-    {
-        alert("Incorrect Name:\n\tPlease use alphabets only.\n\tMinimum length: 1\n\tMaximum Length: 1");
+    if (!NamePattern.test(nameInput.value)) {
+        alert(
+            "Incorrect Name:\n\tPlease use alphabets only.\n\tMinimum length: 1\n\tMaximum Length: 1"
+        );
         return false;
     }
 
-    if(ageInput.value < 10 || ageInput.value > 50)
-    {
+    if (ageInput.value < 10 || ageInput.value > 50) {
         alert("Incorrect Age:\n\tMinimum Age: 10\n\tMaximum Age: 50");
         return false;
     }
